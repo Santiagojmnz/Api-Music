c64422ce2a5d603e5f5bc1bd81f7fa8ece4c60fc
Santiagojmnz <santiag1325@gmail.com>
2022-06-18 01:10:35 -0500

    Api-Music

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict'
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+// cargar rutas
+var user = require('./Routes/user')
+
+//Servidor
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//CORS cabecera
+
+app.use(cors());
+app.options('*', cors());
+
+//Rutas base
+app.use('/api', user);
+
+
+module.exports = app;
