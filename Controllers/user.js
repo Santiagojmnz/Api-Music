d165b56f7d5bcb3213faae9d634a08c64f3aeab5
Luis Franco <josefranco.cbtis16@gmail.com>
2022-06-21 16:39:33 -0500

    Funciones de eliminar y actualizar Usuario

--- Controllers/user.js
+++ Controllers/user.js
@@ -28,32 +28,83 @@ function addUsser(req, res) {
     }
 };
 
-function findUser(req, res){ 
+function findUser(req, res) {
     User.find()
-    .then(user  => {
-        if(user) {
-            res.json(user)
-        } else {
-            res.status(500).send({message: 'No se pudieron cargar los usuarios'})
-        }
-    })
+        .then(user => {
+            if (user) {
+                res.json(user)
+            } else {
+                res.status(500).send({ message: 'No se pudieron cargar los usuarios' })
+            }
+        })
 };
 
-function findUserId(req, res){
+function findUserId(req, res) {
     var userId = req.params.id;
     User.findById(userId)
-    .then(user => {
-        if (user) {
-            res.json(user)
-        } else {
-            res.status(500).send({message: 'No se encontró el usuario'})
-        }
-    })
+        .then(user => {
+            if (user) {
+                res.json(user)
+            } else {
+                res.status(500).send({ message: 'No se encontró el usuario' })
+            }
+        })
+}
+
+function updateUser(req, res) {
+    const params = req.body;
+
+    if (params.name != null && params.surname != null && params.email != null && params.password != null && params.role != null) {
+        const hash = bcrypt.hashSync(params.password, 10);
+        params.password = hash;
+        User.find({ _id: { $ne: req.params.id }, email: params.email })
+            .then((isEmail) => {
+                if (isEmail.length) {
+
+                    return res.status(500).send({ message: 'Email ingresado se encuentra en uso' });
+
+                }
+
+                User.findByIdAndUpdate({ _id: req.params.id }, params)
+                    .then((user) => {
+                        if (user) {
+                            return res.status(200).send({ message: 'Usuario actualizado' });
+                        } else {
+                            return res.status(500).send({ message: 'Usuario no encontrado' });
+                        }
+
+                    }).catch((err) => {
+                        return res.status(500).send({ message: 'Error al procesar la peticion' });
+
+                    });
+
+
+            })
+
+    } else {
+        res.status(500).send({ message: 'Por favor ingrese los campos obligatorios faltantes' })
+    }
+};
+
+function deleteUser(req, res) {
+    User.findByIdAndDelete(req.params.id)
+        .then((user) => {
+            if (user) {
+                res.status(200).send({ message: 'Cuenta eliminada' });
+            } else {
+                res.status(500).send({ message: 'Problemas al eliminar la cuenta' });
+            }
+        }).catch((err) => {
+            res.status(500).send({ message: 'Cuenta no encontrada' });
+
+        });
 }
 
 
 module.exports = {
-    addUsser, 
+    addUsser,
     findUser,
-    findUserId
+    findUserId,
+    updateUser,
+    deleteUser
 }
--- Routes/user.js
+++ Routes/user.js
@@ -6,6 +6,8 @@ const userController = require('../Controllers/user')
 
 api.post('/new-user', userController.addUsser);
 api.get('/find-user', userController.findUser);
-api.get('/find-user/:id', userController.findUserId)
+api.get('/find-user/:id', userController.findUserId);
+api.put('/update-user/:id', userController.updateUser);
+api.delete('/delete-user/:id', userController.deleteUser);
 
 module.exports = api;
