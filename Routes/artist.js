d98b7c37b2696dd555f8c551019af6eea975455a
lynnegj <linegj01@gmail.com>
2022-07-06 05:16:50 -0500

    Corrección de errores

--- Routes/artist.js
+++ Routes/artist.js
@@ -9,5 +9,8 @@ const artistController = require('../Controllers/artistControllers/index');
 api.post('/new-artist', artistController.artistRegister);
 api.put('/update-artist/:id', artistController.updateArtist);
 api.delete('/delete-artist/:id', artistController.deleteArtist);
+api.get('/find-artist', artistController.findArtist);
+api.get('/find-artist/:id', artistController.findArtistId);
+api.get('/artist', artistController.artistPaginate);
 
 module.exports = api;
