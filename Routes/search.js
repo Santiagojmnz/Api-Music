7422e5bec09d1234586738ac1f5585fbf76295cd
Santiagojmnz <santiag1325@gmail.com>
2022-11-29 23:42:19 -0600

    search

--- Routes/search.js
+++ Routes/search.js
@@ -4,5 +4,5 @@ const { search, directedSearch } = require('../Controllers/searchController');
 const { ensureAuth } = require('../Middlewares/authenticated');
 const { isValidCollectionSearch } = require('../Middlewares/validar-campos.js');
 api.get('/search/:query?', ensureAuth, search);
-api.get('/search/:collection/:query?', isValidCollectionSearch, ensureAuth, directedSearch);
+api.get('/search-:collection/:query?', isValidCollectionSearch, ensureAuth, directedSearch);
 module.exports = api;
